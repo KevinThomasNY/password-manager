55aaa205981d3c6373eab88264ed2c2e96e594e5
Kevin Thomas <kevinthomasny@gmail.com>
2024-09-24 21:11:45 -0400

    fix: update schema

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,9 +1,26 @@
+import { sql } from "drizzle-orm";
 import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
   id: integer("id").primaryKey(),
   userName: text("username", { length: 50 }).notNull().unique(),
-  password: text("password", { length: 50 }).notNull(),
+  password: text("password", { length: 256 }).notNull(),
   firstName: text("first_name", { length: 50 }).notNull(),
   lastName: text("last_name", { length: 50 }).notNull(),
+  createdAt: text("created_at")
+    .notNull()
+    .default(sql`(current_timestamp)`),
+});
+
+export const passwords = sqliteTable("passwords", {
+  id: integer("id").primaryKey(),
+  name: text("name", { length: 100 }).notNull(),
+  password: text("password", { length: 256 }).notNull(),
+  image: text("image", { length: 256 }),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  createdAt: text("created_at")
+    .notNull()
+    .default(sql`(current_timestamp)`),
 });
